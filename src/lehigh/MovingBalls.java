6fb37562116a3bf97a50d32b11a8ee17d277ed9e
Li Zha <liz224@lehigh.edu>
2020-11-25 11:48:02 +0800

    week9 assignment

--- src/lehigh/MovingBalls.java
+++ src/lehigh/MovingBalls.java
@@ -2,7 +2,9 @@ package lehigh;
 
 import processing.core.PApplet;
 
+
 import java.util.Arrays;
+
 
 public class MovingBalls extends PApplet {
     int howManyBalls = 8;
@@ -14,43 +16,50 @@ public class MovingBalls extends PApplet {
     int radius = 30;
 
 
-
-    public void settings(){
+    public void settings() {
 
         size(500, 500);
         Arrays.fill(speedX, 5);
-        Arrays.fill(speedY,5);
-        for (int i = 0; i < howManyBalls; i++){
-            x[i] = random(radius, width-radius);
-            y[i] = random(radius, height-radius);
-            color[i] = new float[]{random(255), random(255),random(255)};
+        Arrays.fill(speedY, 5);
+        for (int i = 0; i < howManyBalls; i++) {
+            x[i] = random(radius, width - radius);
+            y[i] = random(radius, height - radius);
+            color[i] = new float[]{random(255), random(255), random(255)};
         }
 
 
     }
 
-    public void draw(){
+    public void draw() {
         background(0);
-        noStroke();
-        fill(255,50,50);
-        for(int i = 0; i < howManyBalls; i++){
-            fill(color[i][0],color[i][1],color[i][2]);
+        fill(255, 50, 50);
+        for (int i = 0; i < howManyBalls; i++) {
+            fill(color[i][0], color[i][1], color[i][2]);
             ellipse(x[i], y[i], radius * 2, radius * 2);
-            if ((x[i] - radius) <= 0  || (x[i] + radius) >= width) speedX[i] = -speedX[i];
+            if ((x[i] - radius) <= 0 || (x[i] + radius) >= width) speedX[i] = -speedX[i];
 
-            if ((y[i] - radius) <= 0  || (y[i] + radius) >= width) speedY[i] = -speedY[i];
+            if ((y[i] - radius) <= 0 || (y[i] + radius) >= width) speedY[i] = -speedY[i];
 
             x[i] += speedX[i];
             y[i] += speedY[i];
+
+            for (int j = 0; j < howManyBalls; j++) {
+                float X = Math.abs(x[i] - x[j]);
+                float Y = Math.abs(y[i] - y[j]);
+
+                if ((X <= 60) && Y <= 60)
+                    line(x[i], y[i], x[j], y[j]);
             }
+
+
+        }
+    }
+
+
+        public static void main (String[]args){
+            String[] processingArgs = {"MovingBalls"};
+            MovingBalls movingBalls = new MovingBalls();
+            PApplet.runSketch(processingArgs, movingBalls);
         }
 
-
-
-
-    public static void main(String[] args) {
-        String[] processingArgs = {"MovingBalls"};
-        MovingBalls movingBalls = new MovingBalls();
-        PApplet.runSketch(processingArgs, movingBalls);
-    }
 }
--- src/lehigh/MovingBalls2.java
+++ src/lehigh/MovingBalls2.java
@@ -2,17 +2,44 @@ package lehigh;
 
 import processing.core.PApplet;
 
+import java.util.ArrayList;
+import  java.util.Random;
+
 public class MovingBalls2 extends PApplet{
+    class Ball{
+        int x, y, radius;
+        float speedx, speedy, directionx, directiony;
+        float [] color;
+
+        Ball(){
+            radius = (int) random(5,20);
+            x = (int) random(width - radius/2);
+            y = (int) random(width - radius/2);
+            speedx = random(2.0f);
+            speedy = random(2.0f);
+            color = new float[]{random(1.0f), random(1.0f), random(1.0f)};
+
+        }
+
+    }
+ArrayList<Ball> balls;
+
 
 
     public void settings(){
         size(400,400);
+        balls = new ArrayList<>();
+        for(int i = 0; i < 100; i++) {
+            balls.add(new Ball());
+        }
     }
 
-    public void draw(){
+    public void draw() {
         background(0);
         fill(255);
-        ellipse(200,200,20,20);
+        for (Ball ball : balls) {
+            ellipse(ball.x, ball.y, ball.radius, ball.radius);
+        }
     }
 
     public static void main(String[] args) {
